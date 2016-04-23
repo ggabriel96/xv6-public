2bab6cee78b951cc0dc9d138e34bcb2009fb759e
Gabriel Galli <g7.galli96@gmail.com>
2016-04-22 23:31:14 -0300

    Fixed lotterytests logic and added LaTeX report

--- lotterytests.c
+++ lotterytests.c
@@ -3,8 +3,8 @@
 #include "user.h"
 
 #define STDOUT 1
-#define ITERATIONS 112
-#define MAXPROC NPROC
+#define ITERATIONS 10
+#define MAXPROC NPROC / 2
 #define TIMEOUT 112345678
 
 #define PLURAL(x) (((x) == 0 || (x) > 1) ? 1 : 0)
@@ -12,11 +12,11 @@
 int
 main(int argc, char *argv[])
 {
-  int t, i, j, first, pid, fpid, lpid;
-  int tickets[NPROC], firstcount[NPROC];
+  int t, i, j, first, pid, maxticks;
+  int firstcount[MAXTICKS], tickets[NPROC];
   memset(firstcount, 0, sizeof firstcount);
   for (t = 1; t <= ITERATIONS; t++) {
-    fpid = 0; first = 1;
+    first = 1;
     for (i = 1; i <= MAXPROC; i++) {
       if ((pid = fork(i)) == 0) {
         // child code
@@ -25,8 +25,7 @@ main(int argc, char *argv[])
       }
       else if (pid > 0) {
         // parent code
-        lpid = pid; // last pid
-        if (fpid == 0) fpid = pid; // first pid
+        maxticks = i;
         tickets[pid - 1] = i; // tickets range from 0 to NPROC - 1, pids from 1 to NPROC
         printf(STDOUT, "fork %d succeeded with pid %d and %d ticket%s\n", i, pid, tickets[pid - 1], PLURAL(tickets[pid - 1]) ? "s" : "");
       }
@@ -35,16 +34,15 @@ main(int argc, char *argv[])
     }
     while ((pid = wait()) != -1) {
       if (first) {
-        firstcount[pid - 1]++;
+        // storing how many times a process with X tickets
+        // finished first, rather than with pid Y
+        firstcount[tickets[pid - 1] - 1]++;
         first = 0;
       }
       printf(STDOUT, "child with pid %d and %d ticket%s done\n", pid, tickets[pid - 1], PLURAL(tickets[pid - 1]) ? "s" : "");
     }
   }
-  printf(STDOUT, "fpid %d, lpid %d\n", fpid, lpid);
-//  for (i = 0; i < NPROC; i++)
-//    printf(STDOUT, "child with pid %d finished first %d time%s\n", i + 1, firstcount[i], PLURAL(firstcount[i - 1]) ? "s" : "");
-  for (i = fpid; i <= lpid; i++)
-    printf(STDOUT, "child with pid %d finished first %d time%s\n", i, firstcount[i - 1], PLURAL(firstcount[i - 1]) ? "s" : "");
+  for (i = 0; i < maxticks; i++)
+    printf(STDOUT, "process with %d tickets finished first %d time%s\n", i + 1, firstcount[i], PLURAL(firstcount[i]) ? "s" : "");
   exit();
 }
