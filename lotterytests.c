ae1828588a76947cce68cb800ebc17dca9e291c7
Gabriel Galli <g7.galli96@gmail.com>
2016-04-23 00:17:21 -0300

    Improved lottery tests. Final version?

--- lotterytests.c
+++ lotterytests.c
@@ -3,31 +3,41 @@
 #include "user.h"
 
 #define STDOUT 1
-#define ITERATIONS 10
+#define ITERATIONS 11
 #define MAXPROC NPROC / 2
 #define TIMEOUT 112345678
 
 #define PLURAL(x) (((x) == 0 || (x) > 1) ? 1 : 0)
 
+unsigned long randstate = 1;
+unsigned long
+rand()
+{
+  randstate = randstate * 1664525 + 1013904223;
+  return randstate;
+}
+
 int
 main(int argc, char *argv[])
 {
-  int t, i, j, first, pid, maxticks;
   int firstcount[MAXTICKS], tickets[NPROC];
+  int t, i, j, first, pid, ticks, maxtick, mintick;
   memset(firstcount, 0, sizeof firstcount);
-  for (t = 1; t <= ITERATIONS; t++) {
+  mintick = MAXTICKS + 1; maxtick = MINTICKS - 1;
+  for (t = 0; t < ITERATIONS; t++) {
     first = 1;
     for (i = 1; i <= MAXPROC; i++) {
-      if ((pid = fork(i)) == 0) {
+      ticks = rand() % MAXTICKS + 1;
+      if ((pid = fork(ticks)) == 0) {
         // child code
         for (j = 0; j < TIMEOUT; j++);
         exit();
       }
       else if (pid > 0) {
         // parent code
-        maxticks = i;
-        tickets[pid - 1] = i; // tickets range from 0 to NPROC - 1, pids from 1 to NPROC
-        printf(STDOUT, "fork %d succeeded with pid %d and %d ticket%s\n", i, pid, tickets[pid - 1], PLURAL(tickets[pid - 1]) ? "s" : "");
+        mintick = min(ticks, mintick); maxtick = max(ticks, maxtick);
+        tickets[pid - 1] = ticks;
+        printf(STDOUT, "fork %d succeeded with pid %d and %d ticket%s\n", i, pid, ticks, PLURAL(ticks) ? "s" : "");
       }
       else
         printf(STDOUT, "fork %d failed\n", i);
@@ -42,7 +52,10 @@ main(int argc, char *argv[])
       printf(STDOUT, "child with pid %d and %d ticket%s done\n", pid, tickets[pid - 1], PLURAL(tickets[pid - 1]) ? "s" : "");
     }
   }
-  for (i = 0; i < maxticks; i++)
-    printf(STDOUT, "process with %d tickets finished first %d time%s\n", i + 1, firstcount[i], PLURAL(firstcount[i]) ? "s" : "");
+  printf(STDOUT, "mintick: %d, maxtick: %d\n", mintick, maxtick);
+  for (ticks = mintick; ticks <= maxtick; ticks++)
+    if (firstcount[ticks - 1] > 0)
+      printf(STDOUT, "processes with %d ticket%s finished first %d time%s\n", ticks, PLURAL(ticks) ? "s" : "",
+            firstcount[ticks - 1], PLURAL(firstcount[ticks - 1]) ? "s" : "");
   exit();
 }
